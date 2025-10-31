65c889e6e038b26e206ee34f277e225c0f05ebb2
Černoliška Vuková <singasarska@gmail.com>
2025-10-31 19:57:59 +0700

    Add user-and-password.py for API access

---
+++ core/user-and-password.py
@@ -0,0 +1,7 @@
+# user-and-password.py
+# Only compatible if you use your main account (not bot account)
+
+API_URL = "https://(your target wiki)/w/api.php"
+USERNAME = "Example@Example"
+PASSWORD = "BotPassword"
+USER_AGENT = "FoxwikibotOS/1.0"
